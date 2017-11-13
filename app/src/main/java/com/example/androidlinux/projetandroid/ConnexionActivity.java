f8afc51389b275035240ce6740eef08140589868
Hugo Mehdaoui <hugo.mehdaoui@gmail.com>
2017-11-13 19:27:16 -0400

    ajout pc Hugo

---
+++ app/src/main/java/com/example/androidlinux/projetandroid/ConnexionActivity.java
@@ -0,0 +1,15 @@
+package com.example.androidlinux.projetandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConnexionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connexion);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/androidlinux/projetandroid/MyItem.java
@@ -0,0 +1,31 @@
+package com.example.androidlinux.projetandroid;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.maps.android.clustering.ClusterItem;
+
+/**
+ * Created by androidlinux on 10/11/17.
+ */
+
+public class MyItem implements ClusterItem {
+
+    private int lat;
+    private int lng;
+
+    private final LatLng Position;
+
+    public MyItem(double lat, double lng) {
+
+        Position = new LatLng(lat, lng);
+    }
+
+    public MyItem(LatLng position) {
+        this.Position = position;
+    }
+
+    @Override
+    public LatLng getPosition() {
+
+        return Position;
+    }
+}
