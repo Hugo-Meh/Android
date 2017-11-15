c7ef3fc6f516b2a96517f7c4a109ccf11264f7ed
Hugo Mehdaoui <hugo.mehdaoui@gmail.com>
2017-11-15 08:26:18 -0400

    activity Bravo test BDD

---
+++ app/src/main/java/com/example/androidlinux/projetandroid/Bravo.java
@@ -0,0 +1,25 @@
+package com.example.androidlinux.projetandroid;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import Entities.User;
+import manager.UserManager;
+
+public class Bravo extends AppCompatActivity {
+    Context ctx;
+    TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bravo);
+
+        tv = (TextView) findViewById(R.id.textTest);
+        String infoUserDB;
+        User user;
+        user = UserManager.verifyUser(ctx,"hugo", "abc123");
+        tv.setText(user.getLogin()+" "+user.getPwd());
+    }
+}
